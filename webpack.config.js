86a2ab84810324afc9f570a7cc2c8cbe425a6699
Mohamed Ashry <mohamedashry22@gmail.com>
2021-10-19 16:06:22 +0200

    build docs

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     // devtool: "source-map",
     entry: './src/index.js',
     output: {
